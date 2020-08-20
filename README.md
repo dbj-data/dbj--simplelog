624abf585c6e37f5b4b9197993ee9b00055b3340
dbjdbj <dbj@dbj.org>
2020-08-20 11:23:35 +0200

    improved

--- README.md
+++ README.md
@@ -4,14 +4,41 @@
 > (c) 2019-2020 by dbj.org   -- LICENSE DBJ -- https://dbj.org/license_dbj/ 
 
 Double personality logging. By default all the log goes to 
-the console through `stderr`. And to local file named equal to full application path + ".log".
+the console through `stderr`, **and** to the local file named equal to full application path + ".log".
 Also there is resilience in presence of threads "built in".
 
 > Important: log file is reset on each application run. That is the current policy.
 
+It is on the **roadmap** to offer several setup combinations to use on startup. Including log file opening options
+
 ## How to use
 
-On startup one can use the setup function `dbj_log_setup`. But we recommend to use the `dbj_simple_log_startup(const char* app_full_path)` 
+Through these macros
+
+```cpp
+log_trace(...) ;
+
+log_debug(...) ;
+
+log_info(...);
+
+log_warn(...);
+
+log_error(...) ;
+
+log_fatal(...) ;
+```
+
+Usage syntax is exactly the same as for the `printf` family, format string.
+
+If `DBJ_LOG_USE_COLOR` is defined output is coloured. By default it is.
+
+![coloured view in vs code](in_vs_code.jpg)
+
+NOTE: above is VS Code view which indeed is coloured. But colors are not the same as on Windows 10 console.
+
+### You need to start it
+On start-up one can use the set-up function `dbj_log_setup`. But we recommend to use the `dbj_simple_log_startup(const char* app_full_path)` 
 immediately after main() starts.
 
 ```cpp
@@ -28,8 +55,8 @@ int main( const int argc, char * argv[] )
 ```
 ### How to end properly
 
-This is a Windows lib. And Windows is notorious for being very reluctant to flush. Thus plese make sure at application end
-you place this snippet, somewhere clever as you do:
+This is a Windows lib. And Windows is notorious for being very reluctant to flush. Thus please make sure at application end
+you place this snippet, (somewhere clever as you do):
 
 ```cpp
         FILE* fp_ = dbj_fhandle_log_file_ptr(NULL);
@@ -39,20 +66,25 @@ you place this snippet, somewhere clever as you do:
         if (fp_) { fclose(fp_); fp_ = nullptr; }
 ```
 
+> You must flush to see the log file contents eventually. 
+
+**Roadmap** is to encapsulate this solution inside the library.
+
+
 ## Setup options
 
 | Setup tag  | the effect  |
 |---|---|
- MT | set the Multi Threaded protection
-VT100_CON  | switch on the VT100 console mode, if no coloured output 
-LOG_FROM_APP_PATH  | If app full path is given  use it to obtain log gile name. Make sure you use absoulte paths.
-FILE_LINE_OFF | Exclude file and line, from time stamp 
-SILENT | No console output. Beware, if this is set and no file path is given you will have a completely quiet logging. 
+ DBJ_LOG_MT | set the Multi Threaded protection
+DBJ_LOG_VT100  | switch on the VT100 console mode, if no coloured output 
+DBJ_LOG_TO_APP_PATH  | If app full path is given  use it to obtain log gile name. Make sure you use absolute paths.
+DBJ_LOG_FILE_LINE_OFF | Exclude file and line, from time stamp 
+DBJ_LOG_NO_CONSOLE | No console output. Beware, if this is set and no file path is given you will have no logging. 
 
 
----
+## Building
 
-Built with CL.exe, which means C99 but somewhat undocumented. Roadmap is to switch to clang 10.x and use C11 goodies.
+Built with CL.exe, which in reality means C99, but somewhat undocumented. **Roadmap** is to switch to clang 10.x and use C11 goodies.
 
 How to use: best add it as a git submodule. Include `dbj_simple_log.h` 
 Include it's only C file  `dbj_simple_log.c` in your project. Use.
--- dbj_simple_log.h
+++ dbj_simple_log.h
@@ -37,32 +37,45 @@
 		// lock == false -- unlock
 		typedef void (*log_lock_function_ptr)(void* /*udata*/, bool /*lock*/);
 
-		// uses win32 critial section
-		void  default_protector_function(void* udata, bool lock);
+		// default uses win32 critial section
+		// implemented in here
+		// udata is unused 
+		void  default_protector_function(void* /*udata*/, bool /*lock*/);
 
+		// log levels
 		enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
+		// works but currently unused
+		// primary use is to keep mutex or whatever else 
+		// to be used by user locking function
 		void log_set_udata(void* );
+		// user locking function set here
+		// has to match log_lock_function_ptr signatured, and logic
 		void log_set_lock(log_lock_function_ptr );
-		/* dbj added the second argument, for file name/path */
+
+		/* dbj added the second argument, for full file path */
 		void log_set_fp(FILE* , const char * );
+
 		void log_set_level(int );
 		/* beware: if quiet and no file there is no logging at all */
 		void log_set_quiet(bool );
-		/* 0 do not add file line to log lines stamp, 1 add */
+		/* do not add file + line to log lines stamp, or add */
 		void log_set_fileline( bool  );
-		/**/
+		/*
+		for users to see
+		*/
 		const char * const current_log_file_path();
 
+		// all eventually goes here
 		void log_log(int /*level*/, const char* /*file*/, int /*line*/, const char* /*fmt*/, ...);
 
-	 typedef enum  SETUP {
-		MT = 1 , /* set to Multi Threaded */
-		VT100_CON = 2, /* specificaly switch on the VT100 console mode */
-		LOG_FROM_APP_PATH = 4, /* if app full path is given  use it to obtain log gile name*/
-		FILE_LINE_OFF = 8, /* do not show file line on every log line */
-		SILENT = 16 /* no console output, beware of no file and quiet */
-	} SETUP ;
+	 typedef enum  DBJ_LOG_SETUP_ENUM {
+		DBJ_LOG_MT = 1 , /* set to Multi Threaded */
+		DBJ_LOG_VT100 = 2, /* specificaly switch on the VT100 console mode */
+		DBJ_LOG_TO_APP_PATH = 4, /* if app full path is given  use it to obtain log gile name*/
+		DBJ_LOG_FILE_LINE_OFF = 8, /* do not show file line on every log line */
+		DBJ_LOG_NO_CONSOLE = 16 /* no console output, beware of no file and seting this in the same time */
+	} DBJ_LOG_SETUP_ENUM ;
 
 #undef  DBJ_LOG_IS_SETUP
 #define DBJ_LOG_IS_SETUP(S_, B_) (((int)S_ & (int)B_) != 0)
@@ -75,28 +88,28 @@
 	*/
 
 	inline bool dbj_log_setup
-	(int setup /*= SETUP::VT100_CON */, const char* app_full_path /* = nullptr */
+	(int setup /*= DBJ_LOG_SETUP_ENUM::DBJ_LOG_VT100 */, const char* app_full_path /* = nullptr */
 	) 
 	{
-		if ( DBJ_LOG_IS_SETUP( setup, FILE_LINE_OFF) ) {
+		if ( DBJ_LOG_IS_SETUP( setup, DBJ_LOG_FILE_LINE_OFF) ) {
 			log_set_fileline(false);
 		}
 		else 
 			log_set_fileline(false);
 
-		if (DBJ_LOG_IS_SETUP(setup, VT100_CON)) {
+		if (DBJ_LOG_IS_SETUP(setup, DBJ_LOG_VT100)) {
 				enable_vt_mode();
 		}
 
-		if (DBJ_LOG_IS_SETUP(setup, MT)) {
+		if (DBJ_LOG_IS_SETUP(setup, DBJ_LOG_MT)) {
 			log_set_lock( default_protector_function );
 		}
 
-		if (DBJ_LOG_IS_SETUP(setup, SILENT)) {
+		if (DBJ_LOG_IS_SETUP(setup, DBJ_LOG_NO_CONSOLE)) {
 				log_set_quiet( true );
 #ifdef _DEBUG
 			if (app_full_path == NULL ) {
-				perror(__FILE__ "\nWARNING: SILENT is set, but no log file is requested");
+				perror(__FILE__ "\nWARNING: DBJ_LOG_NO_CONSOLE is set, but no log file is requested");
 			}
 #endif
 		} else 
@@ -107,22 +120,32 @@
 
 	dbj_fhandle log_fh = dbj_fhandle_make(app_full_path);
 
+	// assure file handle is propely opened and set
 	errno_t status = dbj_fhandle_assure( &log_fh );
 
 	assert(status == 0);
 
 	log_set_fp(
-		/*
+/*
 	ATTENTION! file_handle.file_ptr() returns FILE * which is not explicitly closed by this lib
-	it is left to the OS to take care of it?.
-	fdopen mode is "wc"
-	*/
+	please make sure at application end, you place this snippet, (somewhere clever as you do):
+
+	FILE* fp_ = dbj_fhandle_log_file_ptr(NULL);
+	assert(fp_);
+	(void)_flushall();
+		// make sure it is fclose, not close
+	if (fp_) { fclose(fp_); fp_ = nullptr; }
+
+	You must flush to see the log file contents eventually
+*/
 	dbj_fhandle_file_ptr( &log_fh ) , log_fh.name 
 	);
 				return true;
 	} // dbj_log_setup
 
 #undef DBJ_LOG_IS_SETUP
+
+// primary usage is through these
 
 #define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
 #define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
@@ -137,7 +160,7 @@ inline int dbj_simple_log_startup(const char* app_full_path)
 {
 	assert( app_full_path );
 
-	if (! dbj_log_setup ( LOG_FROM_APP_PATH | VT100_CON | FILE_LINE_OFF | MT | SILENT , app_full_path)
+	if (! dbj_log_setup ( DBJ_LOG_TO_APP_PATH | DBJ_LOG_VT100 | DBJ_LOG_FILE_LINE_OFF | DBJ_LOG_MT | DBJ_LOG_NO_CONSOLE , app_full_path)
 		)
 		return EXIT_FAILURE;
 
