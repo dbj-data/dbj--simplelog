e2f207c9b17c0d5ca324c299fd2df038c51161c5
dbjdbj <dbj@dbj.org>
2020-09-08 00:44:53 +0200

    pause added

--- logc/test_for_colors.cmd
+++ logc/test_for_colors.cmd
@@ -7,8 +7,9 @@ cls
 echo [101;93m STYLES [0m
 echo ^<ESC^>[0m [0mReset[0m
 echo ^<ESC^>[1m [1mBold[0m
-echo ^<ESC^>[4m [4mUnderline[0m
 echo ^<ESC^>[7m [7mInverse[0m
+echo ^<ESC^>[4m [4mUnderline[0m
+echo.
 echo.
 echo [101;93m NORMAL FOREGROUND COLORS [0m
 echo ^<ESC^>[30m [30mBlack[0m (black)
@@ -20,6 +21,7 @@ echo ^<ESC^>[35m [35mMagenta[0m
 echo ^<ESC^>[36m [36mCyan[0m
 echo ^<ESC^>[37m [37mWhite[0m
 echo.
+echo.
 echo [101;93m NORMAL BACKGROUND COLORS [0m
 echo ^<ESC^>[40m [40mBlack[0m
 echo ^<ESC^>[41m [41mRed[0m
@@ -29,6 +31,7 @@ echo ^<ESC^>[44m [44mBlue[0m
 echo ^<ESC^>[45m [45mMagenta[0m
 echo ^<ESC^>[46m [46mCyan[0m
 echo ^<ESC^>[47m [47mWhite[0m (white)
+echo.
 echo.
 echo [101;93m STRONG FOREGROUND COLORS [0m
 echo ^<ESC^>[90m [90mWhite[0m
@@ -40,6 +43,7 @@ echo ^<ESC^>[95m [95mMagenta[0m
 echo ^<ESC^>[96m [96mCyan[0m
 echo ^<ESC^>[97m [97mWhite[0m
 echo.
+echo.
 echo [101;93m STRONG BACKGROUND COLORS [0m
 echo ^<ESC^>[100m [100mBlack[0m
 echo ^<ESC^>[101m [101mRed[0m
@@ -50,9 +54,14 @@ echo ^<ESC^>[105m [105mMagenta[0m
 echo ^<ESC^>[106m [106mCyan[0m
 echo ^<ESC^>[107m [107mWhite[0m
 echo.
+echo.
 echo [101;93m COMBINATIONS [0m
 echo ^<ESC^>[31m                     [31mred foreground color[0m
 echo ^<ESC^>[7m                      [7minverse foreground ^<-^> background[0m
 echo ^<ESC^>[7;31m                   [7;31minverse red foreground color[0m
 echo ^<ESC^>[7m and nested ^<ESC^>[31m [7mbefore [31mnested[0m
 echo ^<ESC^>[31m and nested ^<ESC^>[7m [31mbefore [7mnested[0m
+
+@echo.
+@pause
+@echo.
